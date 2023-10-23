54d10430f36aeb954e9cc7f5d4ec60f21c4ab792
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 14:01:43 -0600

    Completed block_07 Vault assignment

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 // to open an important vault. Here is the secret combination:`
 
 const startMessage = `You have received this message because you have been chosen to open an important vault.`;
-const endMessage = `Here is the secret combination:`;
-const totalMessage = startMessage + endMessage;
-//const vaultCombinaiton =
+const endMessage = `<br>Here is the secret combination:`;
+const vaultCombinaiton = `x + y + z`;
+const totalMessage = startMessage + endMessage + vaultCombinaiton;
+
 
 //Assign three variables. Each variable contains the corresponding result of calculation using math operators.
-//Each must equal one of the  three codes of the combination.
+//Each must equal one of the three codes of the combination.
+const x = 2+2+2;
+const y = (4*4)/2;
+const z = (x + y)/(y-x);
+console.log(x);
+console.log(y);
+console.log(z);
 
 //Create a dialogue box displaying the vault codes.
+//document.getElementById("button").onclick = function(onclick){
+    alert("x=6,y=8,z=7");
