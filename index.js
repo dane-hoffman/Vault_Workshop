709f058f34b4764f3ab93542e6aa09196c752433
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 13:32:07 -0600

    Added message using purely js function on html.

---
+++ index.js
@@ -0,0 +1,12 @@
+//Create a string for the user saying: `You have received this message because you have been chosen
+// to open an important vault. Here is the secret combination:`
+
+const startMessage = `You have received this message because you have been chosen to open an important vault.`;
+const endMessage = `Here is the secret combination:`;
+const totalMessage = startMessage + endMessage;
+//const vaultCombinaiton =
+
+//Assign three variables. Each variable contains the corresponding result of calculation using math operators.
+//Each must equal one of the  three codes of the combination.
+
+//Create a dialogue box displaying the vault codes.
