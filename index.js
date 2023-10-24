0ca6f36dd2df13901a7084d9b130888c881ecbb1
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 22:04:15 -0600

    Final commit for vault

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 //Create a string for the user saying: `You have received this message because you have been chosen
 // to open an important vault. Here is the secret combination:`
+const userMessage = "You have received this message because you have been chosen to open an important vault.<br>Here is the secret combination: x + y + z";
 
-const startMessage = `You have received this message because you have been chosen to open an important vault.`;
-const endMessage = `<br>Here is the secret combination:`;
-const vaultCombinaiton = `x + y + z`;
-const totalMessage = startMessage + endMessage + vaultCombinaiton;
+function displayMessage() {
+    const messageSection = document.getElementById("userMessage");
+    messageSection.innerHTML = userMessage;
+}
 
+window.addEventListener("load", displayMessage);
 
 //Assign three variables. Each variable contains the corresponding result of calculation using math operators.
 //Each must equal one of the three codes of the combination.
-const x = 2+2+2;
-const y = (4*4)/2;
-const z = (x + y)/(y-x);
-console.log(x);
-console.log(y);
-console.log(z);
+const x = 2 + 2 + 2;
+const y = (4 * 4) - (3 * 3);
+const z = (x + y) / (y - x);
+const vaultCombination = x + y + z;
 
 //Create a dialogue box displaying the vault codes.
 let button = document.getElementById("clickButton");
-button.addEventListener('click',function(){
-    alert ('The code is' + vaultCombinaiton)
+button.addEventListener('click', function () {
+    alert('The answer is: ' + vaultCombination);
 });
